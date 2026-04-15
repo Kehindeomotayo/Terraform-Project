1659d556a9fb5a97141347a3790b1d09df6c1a08
OMOTAYO KEHINDE OLADIMEJI <141505959+Kehindeomotayo@users.noreply.github.com>
2026-04-15 08:43:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -415,6 +415,64 @@ Resources:
 
 
 
+AWS account
+	
+AWS Region
+	
+Stack ID
+	
+Detailed status
+	
+Last operation ID
+	
+Status Reason
+	
+Drift status
+	
+Last drift check time
+
+AWS account
+	
+AWS Region
+	
+Stack ID
+	
+Detailed status
+	
+Last operation ID
+	
+Status Reason
+	
+Drift status
+	
+Last drift check time
+
+461164599838
+us-east-1
+arn:aws:cloudformation:us-east-1:461164599838:stack/StackSet-Bg-PlatformMonitoringServer-StackSet-5b33c90a-db33-4d65-84a1-9e54d95ad891/906e94a0-3819-11f1-8e28-0ea5af7cf5ad
+CANCELLED
+709e3203-af06-6af7-0151-bb0d37c363a0
+Cancelled since failure tolerance has exceeded
+NOT_CHECKED
+-
+751519514498
+us-east-1
+arn:aws:cloudformation:us-east-1:751519514498:stack/StackSet-Bg-PlatformMonitoringServer-StackSet-b9daaefc-2523-4e7a-a95d-a9e03a117a11/67226040-3819-11f1-a412-12ae37ae6ea1
+INOPERABLE
+709e3203-af06-6af7-0151-bb0d37c363a0
+ResourceLogicalId:BgServerUser, ResourceType:AWS::IAM::User, ResourceStatusReason:Resource handler returned message: "Cannot delete entity, must delete login profile first. (Service: Iam, Status Code: 409, Request ID: a555e2a2-5579-43a4-8eaa-ca10515f04de) (SDK Attempt Count: 1)" (RequestToken: 29bfa3b1-3b04-99d0-8cd6-20833cc087cc, HandlerErrorCode: GeneralServiceException).
+NOT_CHECKED
+-
+959563671843
+us-east-1
+arn:aws:cloudformation:us-east-1:959563671843:stack/StackSet-Bg-PlatformMonitoringServer-StackSet-f9d73823-9eaf-4b38-b928-42b292abb362/130af8a0-3819-11f1-b052-126de7bacae5
+CANCELLED
+709e3203-af06-6af7-0151-bb0d37c363a0
+Cancelled since failure tolerance has exceeded
+
+
+
+
 
 
 
